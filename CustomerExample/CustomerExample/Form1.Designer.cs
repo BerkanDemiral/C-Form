89c6f71321681a708a0e48960cbcb7c69d74d377
BerkanDemiral <alfonso.brkn@gmail.com>
2021-10-31 15:11:55 +0300

    project completed

---
+++ CustomerExample/CustomerExample/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace CustomerExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtCustomerNo = new System.Windows.Forms.TextBox();
+            this.TxtName = new System.Windows.Forms.TextBox();
+            this.TxtSurname = new System.Windows.Forms.TextBox();
+            this.TxtPhone = new System.Windows.Forms.MaskedTextBox();
+            this.TxtDate = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.BtnUpdate = new System.Windows.Forms.Button();
+            this.BtnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Müşteri No: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Adı: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Soyadı: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(59, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Doğum Tarihi: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(59, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Ttelefonu: ";
+            // 
+            // TxtCustomerNo
+            // 
+            this.TxtCustomerNo.Enabled = false;
+            this.TxtCustomerNo.Location = new System.Drawing.Point(185, 38);
+            this.TxtCustomerNo.Name = "TxtCustomerNo";
+            this.TxtCustomerNo.Size = new System.Drawing.Size(175, 20);
+            this.TxtCustomerNo.TabIndex = 5;
+            // 
+            // TxtName
+            // 
+            this.TxtName.Location = new System.Drawing.Point(185, 76);
+            this.TxtName.Name = "TxtName";
+            this.TxtName.Size = new System.Drawing.Size(175, 20);
+            this.TxtName.TabIndex = 6;
+            // 
+            // TxtSurname
+            // 
+            this.TxtSurname.Location = new System.Drawing.Point(185, 115);
+            this.TxtSurname.Name = "TxtSurname";
+            this.TxtSurname.Size = new System.Drawing.Size(175, 20);
+            this.TxtSurname.TabIndex = 7;
+            // 
+            // TxtPhone
+            // 
+            this.TxtPhone.Location = new System.Drawing.Point(185, 204);
+            this.TxtPhone.Mask = "(999) 000-0000";
+            this.TxtPhone.Name = "TxtPhone";
+            this.TxtPhone.Size = new System.Drawing.Size(175, 20);
+            this.TxtPhone.TabIndex = 8;
+            this.TxtPhone.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
+            // 
+            // TxtDate
+            // 
+            this.TxtDate.Location = new System.Drawing.Point(185, 158);
+            this.TxtDate.Name = "TxtDate";
+            this.TxtDate.Size = new System.Drawing.Size(175, 20);
+            this.TxtDate.TabIndex = 9;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 312);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 199);
+            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEnter);
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.Location = new System.Drawing.Point(496, 34);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(75, 23);
+            this.BtnAdd.TabIndex = 11;
+            this.BtnAdd.Text = "EKLE";
+            this.BtnAdd.UseVisualStyleBackColor = true;
+            this.BtnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // BtnUpdate
+            // 
+            this.BtnUpdate.Location = new System.Drawing.Point(496, 155);
+            this.BtnUpdate.Name = "BtnUpdate";
+            this.BtnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.BtnUpdate.TabIndex = 12;
+            this.BtnUpdate.Text = "GÜNCELLE";
+            this.BtnUpdate.UseVisualStyleBackColor = true;
+            this.BtnUpdate.Click += new System.EventHandler(this.BtnUpdate_Click);
+            // 
+            // BtnDelete
+            // 
+            this.BtnDelete.Location = new System.Drawing.Point(496, 92);
+            this.BtnDelete.Name = "BtnDelete";
+            this.BtnDelete.Size = new System.Drawing.Size(75, 23);
+            this.BtnDelete.TabIndex = 13;
+            this.BtnDelete.Text = "SİL";
+            this.BtnDelete.UseVisualStyleBackColor = true;
+            this.BtnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(853, 544);
+            this.Controls.Add(this.BtnDelete);
+            this.Controls.Add(this.BtnUpdate);
+            this.Controls.Add(this.BtnAdd);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.TxtDate);
+            this.Controls.Add(this.TxtPhone);
+            this.Controls.Add(this.TxtSurname);
+            this.Controls.Add(this.TxtName);
+            this.Controls.Add(this.TxtCustomerNo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtCustomerNo;
+        private System.Windows.Forms.TextBox TxtName;
+        private System.Windows.Forms.TextBox TxtSurname;
+        private System.Windows.Forms.MaskedTextBox TxtPhone;
+        private System.Windows.Forms.DateTimePicker TxtDate;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.Button BtnUpdate;
+        private System.Windows.Forms.Button BtnDelete;
+    }
+}
+
---
+++ CustomerExample/CustomerExample/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CustomerExample
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection connection;
+        SqlCommand command;
+        SqlDataAdapter adapter;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void GetCustomers()
+        {
+            connection = new SqlConnection("Data Source=DESKTOP-NOQBATS;Initial Catalog=DatabaseExample;Integrated Security=True");
+            connection.Open();
+            adapter = new SqlDataAdapter("Select * from customer", connection);
+
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+            connection.Close();
+
+            // dataGridView'in prop. kısmında "AutoSizeColumnsMode" kısmını fill yparsak dolu olarak gelir.
+            // dataGridView -> "selectionMode" > fullmodeselected dersek bir değer seçince tüm satır seçilip okunabilir.
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetCustomers();
+        }
+
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            // Bir veriye tıklandığı zaman olacakları burada kodlayabiliyoruz. 
+            TxtCustomerNo.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            TxtName.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            TxtSurname.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            TxtDate.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            TxtPhone.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            string query = "insert into customer(name,surname,date,tel) values (@name,@surname,@date,@tel)";
+            command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@name", TxtName.Text);
+            command.Parameters.AddWithValue("@surname", TxtSurname.Text);
+            command.Parameters.AddWithValue("@date", TxtDate.Value);
+            command.Parameters.AddWithValue("@tel", TxtPhone.Text);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            GetCustomers();
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            string query = "delete from customer where mno=@mno";
+            command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@mno", Convert.ToInt32(TxtCustomerNo.Text));
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            GetCustomers();
+        }
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            string query = "update customer set name=@name, surname=@surname, date=@date, tel=@tel where mno=@mno";
+            command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@mno", Convert.ToInt32(TxtCustomerNo.Text));
+            command.Parameters.AddWithValue("@name", (TxtName.Text));
+            command.Parameters.AddWithValue("@surname", (TxtSurname.Text));
+            command.Parameters.AddWithValue("@date", (TxtDate.Value));
+            command.Parameters.AddWithValue("@tel", (TxtPhone.Text));
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            GetCustomers();
+        }
+    }
+}
